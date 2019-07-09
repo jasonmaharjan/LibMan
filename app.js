5d8222e7c49cd1929525e8a27e3389a4caf117e9
Jason <jason8maharjan@gmail.com>
2019-07-09 18:52:39 +0545

    First Commits

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const exphbs = require('express-handlebars');
+
+
+var con = mysql.createConnection({
+  host     : 'localhost',
+	user     : 'root',
+	password : '#jimmypage8877#',
+	database : 'bookit'
+});
+
+con.connect(function(err){
+    if(!err) {
+        console.log("Database is connected");
+    } else {
+        console.log("Error while connecting with database");
+    }
+});
+
+
+// Init App
+const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.json({type:'application/json'}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
+// Route Files
+let books = require('./routes/books');
+let users = require('./routes/users');
+let index = require('./routes/index');
+
+app.use('/', index);
+app.use('/books', books);
+app.use('/users', users);
+
+
+// Start Server
+app.listen(8000, function(){
+  console.log('Server started on port 8000');
+});
---
+++ routes/users.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+// Register Form
+
+router.get('/register', function(req, res){
+  res.render('register');
+});
+
+// Register Proccess
+router.post('/register', function(req, res) {
+
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var password_confirm = req.body.password_confirm;
+
+  req.checkBody('username', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password_confirm', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+
+    if (!username){
+      res.send({message:'Username missing'});
+    }
+
+    else if (!email){
+      res.send({message:'Email missing'});
+    }
+
+    else if (!password){
+      res.send({message:'Password missing'});
+    }
+
+    else if (password != password_confirm){
+      res.send({message:'Passwords do not match'});
+    }
+
+    console.log(errors);
+  }
+
+  else{
+
+    // Check if username is already taken
+
+    con.query(
+      "SELECT * FROM users WHERE username = ?", username, function(err, row, field){
+
+        if (err) throw err;
+
+        else if (row.length>0){
+          res.send({'success': false});
+
+          console.log('Username is already taken');
+        }
+
+        // If username is available
+
+        else{
+
+          // Password Encryption
+
+          bcrypt.genSalt(10, function(err, salt){
+            bcrypt.hash(password, salt, function(err, hash){
+
+              if (err) throw err;
+
+              password = hash;
+
+              con.query(
+
+                "INSERT INTO users (username, email, password) VALUES ('" + username + "', '" + email + "', '" + password + "')",
+                 function(err, row, field){
+
+                   if (err) throw err;
+
+                   else {
+                   console.log('1 user-record inserted');
+
+                   res.send({'success': true});
+                   }
+                 }
+              )
+            })
+          })
+        }
+      }
+    )
+  }
+});
+
+// Login Form
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+// Login Process
+router.post('/login', function(req, res, next){
+  passport.authenticate('local', {
+    successRedirect:'/books/home',
+    failureRedirect:'/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// logout
+router.get('/logout', function(req, res){
+  req.logout();
+  req.flash('success', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
